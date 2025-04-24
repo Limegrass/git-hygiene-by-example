596d9c2680b738cafcdf3d1972fbd91bd0ad52ed
James Ni <jnimz@amazon.com>
2025-04-24 16:29:55 +0000

    407c0587-a106-4887-918f-68a7fa486a5d

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /** supper sekrit business logic function */
-pub fn custom_add_2025_04_24_16_29_54_986185501(a: f64, b: f64) -> f64 {
+pub fn custom_add_2025_04_24_16_29_55_002610671(a: f64, b: f64) -> f64 {
     a + b
 }
 
@@ -9,13 +9,13 @@ mod tests {
 
     #[test]
     fn add_values() {
-        assert_eq!(custom_add_2025_04_24_16_29_54_986185501(1.0, 2.0), 3.0)
+        assert_eq!(custom_add_2025_04_24_16_29_55_002610671(1.0, 2.0), 3.0)
     }
 
     #[test]
     fn add_big_values() {
         assert_eq!(
-            custom_add_2025_04_24_16_29_54_986185501(18_446_744_073_709_551_615.0, 1.0), 18_446_744_073_709_551_616.0 // adding to u64::MAX
+            custom_add_2025_04_24_16_29_55_002610671(18_446_744_073_709_551_615.0, 1.0), 18_446_744_073_709_551_616.0 // adding to u64::MAX
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use clap::Parser;
-use git_history_by_example::custom_add_2025_04_24_16_29_54_986185501;
+use git_history_by_example::custom_add_2025_04_24_16_29_55_002610671;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// First number to add
-    first: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_54_986185501;
+    first: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_002610671;
 
     /// Second number to add
-    second: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_54_986185501;
+    second: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_002610671;
 }
 
 fn main() {
     let args = Args::parse();
-    let total = custom_add_2025_04_24_16_29_54_986185501(args.first, args.second);
+    let total = custom_add_2025_04_24_16_29_55_002610671(args.first, args.second);
     println!("{total}");
 }
