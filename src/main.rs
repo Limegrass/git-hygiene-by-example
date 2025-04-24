3c2c95aa6ca2b088b98a41bca432951bcc53a680
James Ni <jnimz@amazon.com>
2025-04-24 00:44:11 +0000

    ce2fa7ef-34b7-4254-9639-52180faed203

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn custom_add(a: i64, b: i64) -> i64 {
+    a + b
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_values() {
+        assert_eq!(custom_add(1, 2), 3)
+    }
 }
