fab299ef9c821f87099c733179a8f9f54f44db23
James Ni <jnimz@amazon.com>
2025-04-24 01:31:23 +0000

    feat: add support for adding negative numbers
    Passing into the CLI is a bit funky with it being passed like
    `1 -- -20000`, especially when running with `cargo run`.
    e.g. `cargo run -- 1 -- -20000`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /** Super secret business logic function */
-pub fn custom_add(a: u128, b: u128) -> u128 {
+pub fn custom_add(a: i128, b: i128) -> i128 {
     a + b
 }
 
@@ -22,5 +22,11 @@ mod tests {
     #[test]
     fn add_float_not_represented_numbers() {
         assert_eq!(custom_add(9007199254740993, 1), 9007199254740994)
+    }
+
+    #[test]
+    fn add_negative_numbers() {
+        assert_eq!(custom_add(18_446_744_073_709_551_615, -1), 18_446_744_073_709_551_614);
+        assert_eq!(custom_add(-18_446_744_073_709_551_615, 1), -18_446_744_073_709_551_614);
     }
 }
