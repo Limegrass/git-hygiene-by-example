016ff2afe6b563a671462e14dbefdbc06d1bfa0d
James Ni <jnimz@amazon.com>
2025-04-24 00:51:52 +0000

    d3bf57c3-0fd2-4e00-9bb5-6c1edb5c2368

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn custom_add(a: i64, b: i64) -> i64 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_values() {
+        assert_eq!(custom_add(1, 2), 3)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-fn custom_add(a: i64, b: i64) -> i64 {
-    a + b
-}
+use git_history_by_example::custom_add;
 
 fn main() {
     let total = custom_add(1, 1);
     println!("Your total is {total}");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_values() {
-        assert_eq!(custom_add(1, 2), 3)
-    }
 }
