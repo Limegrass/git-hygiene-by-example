a337267b007a45ebc8d0033054f0eb4e56f2c360
James Ni <jnimz@amazon.com>
2025-04-24 00:53:38 +0000

    feat: use f64 instead of u64 to add larger numbers
    We received a business requirement to add numbers that are greater
    than u64. This updates the function to use f64
    
    This is the breaking commit that ends up breaking the add_big_values
    unit test. We will want to find this with git.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /** supper sekrit business logic function */
-pub fn custom_add(a: i64, b: i64) -> i64 {
+pub fn custom_add(a: f64, b: f64) -> f64 {
     a + b
 }
 
@@ -9,6 +9,13 @@ mod tests {
 
     #[test]
     fn add_values() {
-        assert_eq!(custom_add(1, 2), 3)
+        assert_eq!(custom_add(1.0, 2.0), 3.0)
+    }
+
+    #[test]
+    fn add_big_values() {
+        assert_eq!(
+            custom_add(18_446_744_073_709_551_615.0, 1.0), 18_446_744_073_709_551_616.0 // adding to u64::MAX
+        )
     }
 }
