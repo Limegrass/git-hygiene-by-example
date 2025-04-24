1bd018bbd9cb108f29c7ecbc1f80f1db25b8213b
James Ni <jnimz@amazon.com>
2025-04-24 16:29:51 +0000

    fdbe1350-dfbb-4d08-a3fe-dd1fb32e5733

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /** supper sekrit business logic function */
-pub fn custom_add_2025_04_24_16_29_51_001152856(a: f64, b: f64) -> f64 {
+pub fn custom_add_2025_04_24_16_29_51_017312457(a: f64, b: f64) -> f64 {
     a + b
 }
 
@@ -9,13 +9,13 @@ mod tests {
 
     #[test]
     fn add_values() {
-        assert_eq!(custom_add_2025_04_24_16_29_51_001152856(1.0, 2.0), 3.0)
+        assert_eq!(custom_add_2025_04_24_16_29_51_017312457(1.0, 2.0), 3.0)
     }
 
     #[test]
     fn add_big_values() {
         assert_eq!(
-            custom_add_2025_04_24_16_29_51_001152856(18_446_744_073_709_551_615.0, 1.0), 18_446_744_073_709_551_616.0 // adding to u64::MAX
+            custom_add_2025_04_24_16_29_51_017312457(18_446_744_073_709_551_615.0, 1.0), 18_446_744_073_709_551_616.0 // adding to u64::MAX
         )
     }
 }
