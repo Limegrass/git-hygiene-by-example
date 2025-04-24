b3a769408f76cfc6b6d562e296044e14cf840a42
James Ni <jnimz@amazon.com>
2025-04-24 16:29:55 +0000

    c7fc88bf-2a1a-4bdc-8e56-9eda146c4234

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use clap::Parser;
-use git_history_by_example::custom_add_2025_04_24_16_29_55_537992726;
+use git_history_by_example::custom_add_2025_04_24_16_29_55_554329484;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// First number to add
-    first: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_537992726;
+    first: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_554329484;
 
     /// Second number to add
-    second: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_537992726;
+    second: f64, // matches type of git_history_by_example::custom_add_2025_04_24_16_29_55_554329484;
 }
 
 fn main() {
     let args = Args::parse();
-    let total = custom_add_2025_04_24_16_29_55_537992726(args.first, args.second);
+    let total = custom_add_2025_04_24_16_29_55_554329484(args.first, args.second);
     println!("{total}");
 }
